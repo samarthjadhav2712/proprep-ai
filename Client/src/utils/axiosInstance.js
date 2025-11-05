6de4206e6fd7165ac16b868f187a262e4e093812
Samarth Jadhav <samarth10jadhav@gmail.com>
2025-11-05 23:19:03 +0530

    final commit 3

--- Client/src/utils/axiosInstance.js
+++ Client/src/utils/axiosInstance.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import { BASE_URL } from './apiPath';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:8000";
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL,
